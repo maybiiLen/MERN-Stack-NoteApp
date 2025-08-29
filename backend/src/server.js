e16781863af88c6e3cd68fd0c85e1954bc399dd9
Henry Pham <henrypham6719@gmail.com>
2025-08-28 21:27:01 -0500

    Finalizing Backend Server and Adding Rate Limiter
    added rate limiter so user cant't send too many request and finish last part of backend server

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,22 +2,24 @@ import express from 'express';
 import notesRoute from "./routes/notesRoutes.js";
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-connectDB()
-
 //middleware
-app.use(express.json());
+app.use(express.json()); // this middleware parses incoming JSON requests
+//our simple custom middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(rateLimiter);
+
 
 app.use("/api/notes", notesRoute);
 
-app.listen(PORT, () => {
-  console.log("Server is running on port :", PORT);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is running on port :", PORT);
+  });
 });
-
-
