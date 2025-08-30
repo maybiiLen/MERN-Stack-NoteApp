ed1dd9730c615a9a490f0f17f0ddcf41ee98ea4d
Henry Pham <henrypham6719@gmail.com>
2025-08-29 23:19:31 -0500

    Create Page
    finish making the craete page and functional

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import RateLimitedUI from "../components/RateLimitedUI";
-import axios from "axios";
+import api from "../components/lib/axios";
 import toast from "react-hot-toast";
 import NoteCard from "../components/NoteCard";
 
@@ -13,7 +13,7 @@ const HomePage = () => {
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const res = await axios.get("http://localhost:3000/api/notes");
+        const res = await api.get("/notes");
         console.log(res.data);
         setNotes(res.data)
         setIsRateLimited(false);
