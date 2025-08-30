f137748f5f9d62b77712d5f1d56739c1577569b0
Henry Pham <henrypham6719@gmail.com>
2025-08-29 23:36:05 -0500

    Finish Delete Functionality
    simple implementation of deleting notes from homepage

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,9 +1,26 @@
 import { formatDate } from "./lib/utils";
 import { PenSquare, Trash2 } from "lucide-react";
 import { Link } from "react-router-dom";
+import api from "./lib/axios";
+import toast from "react-hot-toast";
 
-const NoteCard = ({ note }) => {
+const NoteCard = ({ note, setNotes }) => {
+  const handleDelete = async (e,id) => {
+    e.preventDefault(); //doesnt click on link when clicking on trash icon
+
+    if(!window.confirm("Are you sure you want to delete this note?"))return;
+    
+      try {
+        await api.delete(`/notes/${id}`)
+        setNotes((prev) => prev.filter((note) => note._id !== id)) // get rid of the deleted one
+        toast.success("Note deleted successfully")
+      } catch (error) {
+        toast.error("Error deleting note")
+      }
+  };
+
   return (
+
     <Link to={`/notes/${note._id}`}
       className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-[#00FF9D]"
     >
@@ -11,10 +28,11 @@ const NoteCard = ({ note }) => {
         <h3 className="card-title text-base-content">{note.title}</h3>
         <p className="text-base-content/70 line-clamp-3">{note.content}</p>
         <div className="card-actions justify-between items-center mt-4">
-          <span className="text-sm text-base-content/60">{formatDate(new Date(note.createdAt))}</span>
+          <span className="text-sm text-base-content/60">
+          {formatDate(new Date(note.createdAt))}</span>
           <div className="flex items-center gap-1">
             <PenSquare className="w-4 h-4" />
-            <button className="btn btn-ghost btn-xs text-error">
+            <button className="btn btn-ghost btn-xs text-error" onClick={(e) => handleDelete(e,note._id)}>
               <Trash2 className="w-4 h-4" />
             </button>
           </div>
